d4f54540248643554a8aebc30f6197fb22d08747
sabbirofficial44 <sabbirofficial44@gmail.com>
2026-02-21 18:58:01 +0600

    app.js

--- ecommerce/app.js
+++ ecommerce/app.js
@@ -168,7 +168,9 @@ app.get('*', (req, res) => {
 
 // ৩. পোর্ট সেটিংস (Render এর জন্য process.env.PORT)
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, '0.0.0.0', () => {
-    console.log(`Server running on port ${PORT}`);
-});
+
+app.listen(PORT, () => 
+  console.log(`Server running on port ${PORT}`)
+);
+
 
